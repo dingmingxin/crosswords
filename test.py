952eefc578866941932d65ec80479668bebb8425
dmx <dingmingxin10@gmail.com>
2022-03-06 18:37:05 +0800

    update

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-words = ['БУК', 'КЛУБ', 'КУБ', 'ЛУК']
-clues = ['БУК', 'КЛУБ', 'КУБ', 'ЛУК']
+#words = ['БУК', 'КЛУБ', 'КУБ', 'ЛУК']
+#clues = ['БУК', 'КЛУБ', 'КУБ', 'ЛУК']
+words = ["ЛОТ","ОПТ","ОСЁЛ","ОТЁЛ","ПЛОТ","ПОЛ","ПОЛЁТ","ПОСТ","СТО","СТОЛ","СТОЛП","ТОП"]
+clues = ["ЛОТ","ОПТ","ОСЁЛ","ОТЁЛ","ПЛОТ","ПОЛ","ПОЛЁТ","ПОСТ","СТО","СТОЛ","СТОЛП","ТОП"]
 
 cw = Crossword(words, clues)
 
 tries = 10
 grid = cw.getSquareGrid(tries)
 print(grid)
+
